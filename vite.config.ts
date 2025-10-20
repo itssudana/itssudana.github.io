21f0c04e2ae163a76d40b7782b664f79814ae808
itssudana <sudana.works@gmail.com>
2025-10-20 09:58:42 +0800

    update vite chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
-// Remove this if you don't need overlay
 import { vitePluginErrorOverlay } from "@hiogawa/vite-plugin-error-overlay";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -25,5 +24,25 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base:"/"
+  base: "/",
+
+  build: {
+    chunkSizeWarningLimit: 1000, // ubah batas peringatan jadi 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Pisahkan setiap library di node_modules jadi chunk sendiri
+          if (id.includes("node_modules")) {
+            const parts = id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/");
+            // contoh: node_modules/framer-motion/dist/index.js → framer-motion
+            const name = parts[0].startsWith("@") ? `${parts[0]}/${parts[1]}` : parts[0];
+            return name;
+          }
+        },
+      },
+    },
+  },
 }));
